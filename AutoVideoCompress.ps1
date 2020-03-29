4e2bd119ce9cc7e7e79a9358936086b1a9d37484
Jonahscottwright <43269867+Jonahscottwright@users.noreply.github.com>
2020-03-29 17:15:15 +0200

    V1.1
    1st version. Contains many quirks.

--- AutoVideoCompress.ps1
+++ AutoVideoCompress.ps1
@@ -1,19 +1,3 @@
-#$Script:showWindowAsync = Add-Type -MemberDefinition @"
-#[DllImport("user32.dll")]
-#public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
-#"@ -Name "Win32ShowWindowAsync" -Namespace Win32Functions -PassThru
-#Function Show-Powershell()
-#{
-#$null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 10)
-#}
-#Function Hide-Powershell()
-#{
-#$null = $showWindowAsync::ShowWindowAsync((Get-Process -Id $pid).MainWindowHandle, 2)
-#}
-#
-#Hide-Powershell
-
-
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
